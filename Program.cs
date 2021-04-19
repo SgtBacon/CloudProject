a9947e369ca86fd52301bce9cfe7a8049daf2de4
Jay Helm <45608814+SgtBacon@users.noreply.github.com>
2021-04-19 11:25:09 -0700

    Updated comments

--- Program.cs
+++ Program.cs
@@ -7,43 +7,49 @@ using Newtonsoft.Json;
 
 class Program
 { 
+    //Key and Endpoint provided when generating Resource groups, required to access Translator
     private static readonly string subscriptionKey = "f3c99b4236cd4a7c936d271bc3687768";
     private static readonly string endpoint = "https://api.cognitive.microsofttranslator.com/";
 
-    // Add your location, also known as region. The default is global.
-    // This is required if using a Cognitive Services resource.
+    //Location is required for using Cognitive Services, Global is default
     private static readonly string location = "global";
     
-    static async Task Main(string[] args)
+    void Main(string[] args)
     {
-        Console.WriteLine("Thank you");
-        await Translate();
+        Console.WriteLine("Enter your phrase to be translated"); //Debugging line
+        await Translate();  //Call Translate function
     }
     
+    //Translate function, Asynchronous Task function, allows use of Async and Await
+    //Requires: nothing
+    //Returns: currently nothing
+    //TODO: Add variable to let user change what language is used
+    //TODO: Connect Translator to HTML code for release
     static async Task Translate()
     {
-                // Output languages are defined as parameters, input language detected.
-        string base_url = "https://api.translator.azure";
-        string route = "/translate?api-version=3.0&to=de&to=it"; //This line selects what language to use. "to=de&to=it"
-        string textToTranslate = TextInput();
+        // Output languages are defined as parameters, input language detected.
+        //string base_url = "https://api.translator.azure";
+        string route = "/translate?api-version=3.0&to=de&to=it";        //Delects what language to use. "to=de&to=it"
+        string textToTranslate = TextInput();                           //Input text for translation
         object[] body = new object[] { new { Text = textToTranslate } };
         var requestBody = JsonConvert.SerializeObject(body);
     
+        //Create client/request
         using (var client = new HttpClient())
         using (var request = new HttpRequestMessage())
         {
-            // Build the request.
+            // Build the request to send to Cognitive Services for translation
             request.Method = HttpMethod.Post;
-            request.RequestUri = new Uri(endpoint + route);
-            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
-            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
-            request.Headers.Add("Ocp-Apim-Subscription-Region", location);
+            request.RequestUri = new Uri(endpoint + route);   //Create new Unique Resource Identifier(URI) for HTTPRequestMessage   
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");   //Add StringContent to request for translation
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);  //Add Subscription key info to request
+            request.Headers.Add("Ocp-Apim-Subscription-Region", location);      //Add Location info to Request
             
             // Send the request and get response.
             HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
             // Read response as a string.
             string result = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(result);
+            Console.WriteLine(result);  //Write result, only for debugging
 
         }
     }
